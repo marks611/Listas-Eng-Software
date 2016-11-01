8b637d0766b6f702ededb24633f99134150ad3ee
Giovanni Armane <Giovanni Armane>
2016-11-01 21:40:31 -0200

    Exercício 14 feito.

---
+++ Lista 3/ex14_Persistencia.java
@@ -0,0 +1,127 @@
+package Persistencia;
+
+public class ClienteEspecialDAO {
+	public boolean inserir(ClienteEspecial cliente) {
+		// Código
+	}
+
+	public boolean alterar(ClienteEspecial cliente) {
+		// Código
+	}
+
+	public boolean excluir(ClienteEspecial cliente) {
+		// Código
+	}
+
+	public ClienteEspecial pesquisar(CLienteEspecial cliente) {
+		// Código
+	}
+}
+
+public class MedicamentoDAO {
+	public boolean inserir(Medicamento medic) {
+		// Código
+	}
+
+	public boolean alterar(Medicamento medic) {
+		// Código
+	}
+
+	public boolean excluir(Medicamento medic) {
+		// Código
+	}
+
+	public Medicamento pesquisar(Medicamento medic) {
+		// Código
+	}
+}
+
+public class VendaDAO {
+	public boolean inserir(Venda venda) {
+		// Código
+	}
+
+	public boolean alterar(Venda venda) {
+		// Código
+	}
+
+	public boolean excluir(Venda venda) {
+		// Código
+	}
+
+	public Venda pesquisar(Venda venda) {
+		// Código
+	}
+}
+
+public class UsuarioDAO {
+	public boolean inserir(Usuario usuario) {
+		// Código
+	}
+
+	public boolean alterar(Usuario usuario) {
+		// Código
+	}
+
+	public boolean excluir(Usuario usuario) {
+		// Código
+	}
+
+	public Usuario pesquisar(Usuario usuario) {
+		// Código
+	}
+}
+
+public class CaixaDAO {
+	public boolean inserir(Caixa caixa) {
+		// Código
+	}
+
+	public boolean alterar(Caixa caixa) {
+		// Código
+	}
+
+	public boolean excluir(Caixa caixa) {
+		// Código
+	}
+
+	public Caixa pesquisar(Caixa caixa) {
+		// Código
+	}
+}
+
+public class MoedasDAO {
+	public boolean inserir(Moedas moedas) {
+		// Código
+	}
+
+	public boolean alterar(Moedas moedas) {
+		// Código
+	}
+
+	public boolean excluir(Moedas moedas) {
+		// Código
+	}
+
+	public Moedas pesquisar(Moedas moedas) {
+		// Código
+	}
+}
+
+public class NotasDAO {
+	public boolean inserir(Notas notas) {
+		// Código
+	}
+
+	public boolean alterar(Notas notas) {
+		// Código
+	}
+
+	public boolean excluir(Notas notas) {
+		// Código
+	}
+
+	public Notas pesquisar(Notas notas) {
+		// Código
+	}
+}
