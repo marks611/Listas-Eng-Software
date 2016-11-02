5998beca9c4be1b7bcdd4112c0c1180984439e89
Matheus Santos <msldasilva@gmail.com>
2016-11-02 16:37:43 -0200

    Revisao da revisao

--- Lista 3/ex14_Persistencia_Concretas.java
+++ Lista 3/ex14_Persistencia_Concretas.java
@@ -1,5 +1,14 @@
 package Persistencia
 
+import java.util.list;
+public interface DAO<T> {
+	public boolean inserir(T object);
+	public boolean alterar(T object);
+	public boolean excluir(T object);
+	public T pesquisar(T object);
+	public List<T> listar(String filtro);
+}
+
 public class ClienteEspecialDAO implements DAO<ClienteEspecial> {
 	@Override
 	public boolean inserir(ClienteEspecial cliente) {
